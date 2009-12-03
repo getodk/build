a76974a9c54099c638689a9b375289293de36965
Clint Tseng <clint@dontexplain.com>
2009-12-02 20:58:57 -0800

    frontend/noop: one more soft/hard tabs conversion

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,28 +1,28 @@
 ;(function($)
 {
-	$.fn.spacingTop = function()
-	{
-		var $this = $(this);
-		return parseInt($this.css('margin-top')) + parseInt($this.css('padding-top'));
-	};
+    $.fn.spacingTop = function()
+    {
+        var $this = $(this);
+        return parseInt($this.css('margin-top')) + parseInt($this.css('padding-top'));
+    };
 
-	$.displayText = function(value)
-	{
-		if (value === true)
-			return 'yes';
-		else if (value === false)
-			return 'no';
-		else
-			return value || '&nbsp;'
-	};
+    $.displayText = function(value)
+    {
+        if (value === true)
+            return 'yes';
+        else if (value === false)
+            return 'no';
+        else
+            return value || '&nbsp;'
+    };
 
-	$.live = function(selector, type, callback)
-	{
-		var obj = $([]);
-	    obj.selector = selector;
-	    if (type && callback) {
-	        obj.live(type, callback);
-	    }
-	    return obj;
-	};
+    $.live = function(selector, type, callback)
+    {
+        var obj = $([]);
+        obj.selector = selector;
+        if (type && callback) {
+            obj.live(type, callback);
+        }
+        return obj;
+    };
 })(jQuery);
