4de89775f5a729d82419c2da7d95222d85f46f4f
Clint Tseng <clint@dontexplain.com>
2010-03-25 10:39:53 -0700

    backend/checkpoint: stubbing out methods for user auth and form storage.

--- config.ru
+++ config.ru
@@ -1,18 +1,9 @@
 require 'rubygems'
-require 'cloudkit'
-require 'rufus/tokyo'
 require 'odkmaker_server.rb'
-
-# config
-CloudKit.setup_storage_adapter Rufus::Tokyo::Table.new 'odkmaker.tdb'
 
 # middleware
 use Rack::CommonLogger
 use Rack::Session::Pool
-use CloudKit::OpenIDFilter do |url|
-  (url =~ /^\/(|images|javascripts|stylesheets)/) != nil
-end
 
 # apps
-use CloudKit::Service, :collections => [:forms]
 run Sinatra::Application
--- odkmaker_server.rb
+++ odkmaker_server.rb
@@ -3,10 +3,55 @@ require 'sinatra'
 require 'json'
 require 'rufus/tokyo'
 
+# Rufus::Tokyo::Table.new 'odkmaker.tdb'
+
 # For dev server purposes, load index via Sinatra. Eventually, just do it
 # with Apache.
 get '/' do
   erb :index
 end
 
-# Eventually stuff like parsing images out to base64, etc will go here.
+# Simple RESTful service follows
+
+# Users
+get '/users' do
+  # return 403
+end
+
+post '/users' do
+  # create new user
+end
+
+get '/user/:user_id' do
+  # return user for given id
+end
+
+put '/user/:user_id' do
+  # update user for given id
+end
+
+delete '/user/:user_id' do
+  # delete user for given id
+end
+
+# Forms
+get '/forms' do
+  # return form summaries for authenticated user
+end
+
+post '/forms' do
+  # save new form for authenticated user
+  # (check for name)
+end
+
+get '/form/:form_id'
+  # return form for authenticated user
+end
+
+put '/form/:form_id'
+  # update form for authenticated user
+end
+
+delete '/form/:form_id'
+  # delete form for authenticated user
+end
