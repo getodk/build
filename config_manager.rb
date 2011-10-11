2725d0522ecd9240e4c2317b202eff4a3b3273c4
Clint Tseng <clint@dontexplain.com>
2011-10-11 11:52:12 -0700

    server/noop: typo.

--- config_manager.rb
+++ config_manager.rb
@@ -6,7 +6,7 @@ class ConfigManager
   def self.load
     @@config = (YAML.load_file('config.yml') || {})[ENV['RACK_ENV']]
 
-    throw Exception.new "No valid RACK_ENV supplied for you config! I see: #{ENV['RACK_ENV']}" if @@config.nil?
+    throw Exception.new "No valid RACK_ENV supplied for your config! I see: #{ENV['RACK_ENV']}" if @@config.nil?
   end
 end
 
