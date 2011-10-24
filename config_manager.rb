5302c209c6e9ee5ed6936616ee4ca3c08e32d67a
Clint Tseng <clint@dontexplain.com>
2011-10-24 00:55:43 -0700

    server/noop: don't require dev env specification.

--- config_manager.rb
+++ config_manager.rb
@@ -4,8 +4,9 @@ class ConfigManager
   end
 
   def self.load
-    @@config = (YAML.load_file('config.yml') || {})[ENV['RACK_ENV']]
+    @@config = (YAML.load_file('config.yml') || {})[ENV['RACK_ENV'] || 'development']
 
+    STDERR.write "WARNING: using development env because no RACK_ENV was supplied.\n" unless ENV['RACK_ENV']
     throw Exception.new "No valid RACK_ENV supplied for your config! I see: #{ENV['RACK_ENV']}" if @@config.nil?
   end
 end
