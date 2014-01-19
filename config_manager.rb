818011f14434640d1f0e2fdd8df4bb66960938ad
Clint Tseng <clint@dontexplain.com>
2014-01-18 21:30:52 -0800

    server/bug: ruby now wants require yaml.

--- config_manager.rb
+++ config_manager.rb
@@ -1,10 +1,12 @@
+require 'yaml'
+
 class ConfigManager
   def self.[](config_key)
     return @@config[config_key]
   end
 
   def self.load
-    @@config = (YAML.load_file('config.yml') || {})[ENV['RACK_ENV'] || 'development']
+    @@config = (::YAML.load_file('config.yml') || {})[ENV['RACK_ENV'] || 'development']
 
     STDERR.write "WARNING: using development env because no RACK_ENV was supplied.\n" unless ENV['RACK_ENV']
     throw Exception.new "No valid RACK_ENV supplied for your config! I see: #{ENV['RACK_ENV']}" if @@config.nil?
